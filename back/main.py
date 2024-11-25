838ec4c4114454b9d9486a13791b04e1ba7be8c2
Ivan <letalbark@gmail.com>
2024-11-26 02:39:39 +0700

    fix: CORS in develpoment fix

--- back/main.py
+++ back/main.py
@@ -20,6 +20,10 @@ app.include_router(api_router)
 origins = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
